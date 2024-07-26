e0fdeb854f9f6ee534581a2efa58e7c77c2d4094
dev-hjk <gpwls7357@naver.com>
2024-07-27 07:25:41 +0900

    feat: chat, html, css 수정

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var cors = require('cors')
+
+
 const path = require("path");
 const http = require("http");
 const express = require("express");
@@ -16,19 +19,51 @@ const {
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const is = socketio(server);
+
+// require the socket.io module
+const io = new is(http, {
+  cors: {
+    origin: "http://localhost:8888", // 클라이언트의 주소를 허용
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  },
+});
+
+const port = 3001;
+
+// 미들웨어 설정
+//미들웨어란: 요청과 응답의 중간에 위치하며 클라이언트에게 요청이 오면
+//그 요청에 대한 응답을 보내기 위해 중간에서 목적에 맞게 처리를 해주는 함수
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "http://localhost:8888", // 스프링 서버 주소
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(cors({
+  origin: "http://localhost:8888", // 클라이언트의 주소를 허용
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+  credentials: true,
+}));
+
 const botName = "ChatCord Bot";
 
-(async () => {
-  pubClient = createClient({ url: "redis://127.0.0.1:6379" });
-  await pubClient.connect();
-  subClient = pubClient.duplicate();
-  io.adapter(createAdapter(pubClient, subClient));
-})();
+// (async () => {
+//   pubClient = createClient({ url: "redis://127.0.0.1:6379" });
+//   await pubClient.connect();
+//   subClient = pubClient.duplicate();
+//   io.adapter(createAdapter(pubClient, subClient));
+// })();
 
 // Run when client connects
 io.on("connection", (socket) => {
@@ -82,6 +117,6 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
